21088b289a5fc4aec76207049b3826d54d3a5374
azam alias <azamalias@gmail.com>
2017-12-30 16:07:33 +0800

    separated steps into fruit steps and rest steps

--- features/step_definitions/fruit_steps.rb
+++ features/step_definitions/fruit_steps.rb
@@ -1,12 +1,3 @@
-Given(/^the system knows about the following fruit:$/) do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When(/^the client requests GET \/fruits$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^the response should be JSON:$/) do |string|
+Given(/^the system knows about the following fruit:$/) do |fruits|
   pending # Write code here that turns the phrase above into concrete actions
 end
---
+++ features/step_definitions/rest_steps.rb
@@ -0,0 +1,7 @@
+When(/^the client requests GET \/fruits$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the response should be JSON:$/) do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
