2179c1d7064803159ab054c19f6c695c064fea8a
azam alias <azamalias@gmail.com>
2017-12-30 16:42:01 +0800

    fixed assertion on both sides to ignore whitespaces and newlines

--- features/step_definitions/rest_steps.rb
+++ features/step_definitions/rest_steps.rb
@@ -3,5 +3,5 @@ When(/^the client requests GET (.*)$/) do |path|
 end
 
 Then(/^the response should be JSON:$/) do |json|
-  expect(last_response.body).to eq json
+  expect(JSON.parse(last_response.body)).to eq JSON.parse(json)
 end
