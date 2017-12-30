87c192ef7b8a2efe20cf708016a92ab8674a1b97
azam alias <azamalias@gmail.com>
2017-12-30 16:03:12 +0800

    added generated step definitions from previous cucumber run output

---
+++ features/step_definitions/fruit_steps.rb
@@ -0,0 +1,12 @@
+Given(/^the system knows about the following fruit:$/) do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the client requests GET \/fruits$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the response should be JSON:$/) do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
